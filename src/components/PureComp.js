af5cebeff1759f85f740063a67912a35a5378962
mosharaf456 <mosharaf111hossain@gmail.com>
2024-04-16 16:40:08 +0600

    ref PURE COMPONENT

--- src/components/PureComp.js
+++ src/components/PureComp.js
@@ -6,6 +6,8 @@ class PureComp extends PureComponent {
   // prevent unnecessary re-renders for performance optimization 
   // component will not re-render even if there is a changes if same reference in the state or props so always return new object or array when updating the state or props
   // to re-render the component
+  // It is good idea to use Pure component when you know your component doesn't need to re-render and also all the children components are pure components
+  // to avoid unexpected behavior
   render() {
     return (
       <div>PureComponent {this.props.name}</div>
