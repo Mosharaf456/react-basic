c133082d262ff493d2d05862f0f766136cc9f74a
mosharaf456 <mosharaf111hossain@gmail.com>
2024-04-16 10:02:07 +0600

    ref comment added

--- src/components/Person.js
+++ src/components/Person.js
@@ -4,6 +4,9 @@ function Person({ person }) {
   // const { person } = props            
   //key props is not available in the Person as child component component.  
   // key props is reserved for the parent component. 
+  // key is a special string attribute you need to include when creating lists of elements.
+  // Keys give the elements a stable identity.
+  // keys help React identify which items have changed, are added, or are removed.
   return (
     <div>
         <h2>
