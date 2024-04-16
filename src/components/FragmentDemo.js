4f122faeaf444bbd411766c13a54fc7e683d1a65
mosharaf456 <mosharaf111hossain@gmail.com>
2024-04-16 16:00:50 +0600

    ref react FragmentDemo ADDED

--- src/components/FragmentDemo.js
+++ src/components/FragmentDemo.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 function FragmentDemo() {
+    // React.Fragment is used to wrap multiple elements without adding extra nodes to the DOM , it is mendatory 
+    // to wrap multiple elements in a single parent element
+    // React.Fragment does support key attribute
+    // <> </> does n,t support key attribute
+    // short syntax for React.Fragment is <> </>
   return (
-    <div>FragmentDemo</div>
+    <React.Fragment>
+        <h1>Fragment Demo</h1>
+        <p>This describes the Fragment Demo component</p>
+    </React.Fragment>
   )
 }
 
