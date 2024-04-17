8e4c1e4146f48253e94102d3c5fc880ab553ac4e
mosharaf456 <mosharaf111hossain@gmail.com>
2024-04-17 11:44:11 +0600

    react portals description added

--- src/components/PortalDemo.js
+++ src/components/PortalDemo.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom'
 React portals provide a way to render children into a DOM node that exists outside the DOM hierarchy of the parent component:  
 DOM node that is not under this root element.
 React Portals are used to render components outside the parent component.
+It,s behaves like a normal react component.
 */
 function PortalDemo() {
   return ReactDOM.createPortal(
